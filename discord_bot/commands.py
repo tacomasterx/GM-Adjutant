189f7b9d4087d72c467ebc7df8d9f35cb3bf9406
Tomas Carrillo <tacoman8918@gmail.com>
2024-05-30 12:19:22 -0500

    Added .env functioanlity

--- discord_bot/commands.py
+++ discord_bot/commands.py
@@ -1,13 +1,16 @@
-from pathbuilder import *
+from pathbuilder2 import *
 from display import *
 
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
 import os
 import logging
 
+load_dotenv()
+DISCORD_BOT_SECRET = os.environ["DISCORD_BOT_SECRET"]
 intents = discord.Intents.all()
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents.message_content = True
 
 client = commands.Bot(command_prefix="|", intents=intents)
@@ -22,4 +25,8 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hola krnal")
 
-client.run(f'{os.environ["TDBTOKEN"]}', log_handler=handler, log_level=logging.DEBUG)
+client.run(f'{DISCORD_BOT_SECRET}', log_handler=log_handler, log_level=logging.DEBUG)
+
+@client.command(aliases=['importchar', 'charimport', 'ic'])
+async def importcharacter(ctx):
+    pass
