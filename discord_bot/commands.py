9f0c89676f80cfd78594d207fe97f8dd324ee48f
Tomas Carrillo <tacoman8918@gmail.com>
2024-05-30 12:38:05 -0500

    Added logs to gitignore

--- discord_bot/commands.py
+++ discord_bot/commands.py
@@ -8,7 +8,7 @@ import os
 import logging
 
 load_dotenv()
-DISCORD_BOT_SECRET = os.environ["DISCORD_BOT_SECRET"]
+DISCORD_BOT_SECRET = os.getenv("DISCORD_BOT_SECRET")
 intents = discord.Intents.all()
 log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents.message_content = True
@@ -25,8 +25,11 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hola krnal")
 
-client.run(f'{DISCORD_BOT_SECRET}', log_handler=log_handler, log_level=logging.DEBUG)
 
 @client.command(aliases=['importchar', 'charimport', 'ic'])
 async def importcharacter(ctx):
     pass
+
+
+client.run(f'{DISCORD_BOT_SECRET}', log_handler=log_handler, log_level=logging.DEBUG)
+
